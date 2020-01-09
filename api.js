54119069f8c0244bfe2df53219a9c0f80da294b7
MarkSkits <42118928+MarkSkits@users.noreply.github.com>
2020-01-08 20:04:51 -0500

    Add files via upload

---
+++ api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const Config = require('./config.json');
+
+app.post('/getSymbolInfo', (req, res) => {
+    
+});
+
+if(Config.Server.API.Enabled == true) {
+    app.listen(Config.Server.API.Port, () => console.log("Website enabled!"));
+} else {
+    console.error("API Disabled!");
+    process.exit();
+}
